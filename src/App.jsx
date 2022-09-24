f54341acf18182733a14b324fe001ceb59886caa
br-N <brenobl2000@gmail.com>
2022-09-24 19:46:32 -0300

    Criação dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
 
@@ -7,6 +16,18 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-slate-400 p-6">
         Hello world!
       </h1>
+      <Router>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
     </div>
   )
 }
