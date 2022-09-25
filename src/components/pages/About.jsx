47f90b214ef70dc24922ac5e72432badf13f3348
br-N <brenobl2000@gmail.com>
2022-09-25 20:17:16 -0300

    Criação da página About

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,26 @@
 function About() {
-  return <div></div>;
+  return (
+    <section className="flex items-center justify-center p-4">
+      <div className="p-2">
+        <span className="font-bold text-xl">About me</span>
+        <p className="text-xl">
+          I studied Bachelor's Degree in Interdisciplinary Science and
+          Technology with emphasis in Computer Science at{" "}
+          <span className="font-bold">UNIFESP</span>, Federal University of São
+          Paulo.
+          <br />
+          Furthermore, I was Scientific Initiation Scholarship (InterSCity USP).
+        </p>
+      </div>
+      <div className="p-2">
+        <img
+          className="rounded"
+          src="https://www.unifesp.br/reitoria/dci/images/ICTPatec_1.jpg"
+          alt="Breno Lambertini"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default About;
