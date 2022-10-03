2985d5277565e98972ad92692d0cf93162787705
br-N <brenobl2000@gmail.com>
2022-10-02 22:33:19 -0300

    Ajustes iniciais na página Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,23 @@
 import my_photo from "../../img/me.jpg";
+import humanoid from "../../img/humanoid.gif";
 
 function Home() {
   return (
-    <section className="flex items-center justify-center p-4">
-      <div className="p-2">
-        <img className="rounded-full" src={my_photo} alt="Breno Lambertini" />
+    <section className="flex flex-col items-center w-screen">
+      <div className="flex flex-wrap mt-8 mb-16">
+        <img className="mr-16 mb-4" src={humanoid} alt="Humanoide" />
+
+        <div className="flex flex-col justify-center">
+          <p className="font-bold text-[40px]">Hi there!</p>
+          <p className="text-[30px] my-4">My name is Breno Lambertini.</p>
+          <p className="text-[25px]">
+            I'm a Full Stack Developer looking for a job.
+          </p>
+        </div>
       </div>
-      <div className="p-2">
-        <span className="font-bold text-xl">Hi there!</span>
-        <p className="text-xl">I'm a programmer looking for a job.</p>
+
+      <div className="">
+        <img className="rounded-full" src={my_photo} alt="Breno Lambertini" />
       </div>
     </section>
   );
