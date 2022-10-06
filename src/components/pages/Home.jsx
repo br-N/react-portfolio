8dbefde1944c27e343f33fb140b3297ef25d648a
br-N <brenobl2000@gmail.com>
2022-10-06 15:12:22 -0300

    Ajustes na página Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,56 @@
+import { SiLinkedin, SiGmail, SiGithub } from "react-icons/si";
+
 import my_photo from "../../img/me.jpg";
 import humanoid from "../../img/humanoid.gif";
+import help from "../../img/help-me.gif";
 
 function Home() {
   return (
     <section className="flex flex-col items-center w-screen">
-      <div className="flex flex-wrap mt-8 mb-16">
-        <img className="mr-16 mb-4" src={humanoid} alt="Humanoide" />
+      <div className="flex flex-wrap my-12">
+        <img
+          className="h-72 rounded-full mr-8"
+          src={my_photo}
+          alt="Breno Lambertini"
+        />
 
         <div className="flex flex-col justify-center">
-          <p className="font-bold text-[40px]">Hi there!</p>
-          <p className="text-[30px] my-4">My name is Breno Lambertini.</p>
-          <p className="text-[25px]">
-            I'm a Full Stack Developer looking for a job.
+          <p className="text-[35px]">Hi!</p>
+          <p className="text-[35px] my-4">My name is Breno Lambertini.</p>
+          <p className="font-bold text-[45px] mt-4">
+            I'm a Full Stack Developer.
           </p>
         </div>
       </div>
 
-      <div className="">
-        <img className="rounded-full" src={my_photo} alt="Breno Lambertini" />
+      <div className="flex my-12">
+        <p className="flex items-center mr-8 text-[32px]">CONTACT ME:</p>
+
+        <ul className="flex items-center mt-2">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/brenolambertini/"
+              target="_blank"
+            >
+              <SiLinkedin size={60} />
+            </a>
+          </li>
+          <li className="mx-8">
+            <a href="mailto:brenobl2000@gmail.com" target="_blank">
+              <SiGmail size={60} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/br-N/" target="_blank">
+              <SiGithub size={60} />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="my-8">
+        <img className="" src={help} alt="Help me" />
+        <img className="" src={humanoid} alt="Humanoide" />
       </div>
     </section>
   );
