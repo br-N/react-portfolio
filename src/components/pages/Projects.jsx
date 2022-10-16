92b12c95a8a274b13474c8f994f97057937d1b5a
br-N <brenobl2000@gmail.com>
2022-10-16 16:00:54 -0300

    Adição do projeto 'My Portfolio' e Ajustes gerais

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,13 +1,14 @@
 import culturaeduca from "../../img/culturaeduca.png";
 import robot_of_love from "../../img/robot-of-love.png";
+import portfolio from "../../img/portfolio.png";
 
 function Projects() {
   return (
     <section className="flex flex-col items-center w-screen">
-      <div className="flex flex-wrap max-w-4xl p-3 bg-blue rounded">
+      <div className="flex flex-wrap max-w-4xl mb-16 p-3 bg-blue rounded">
         <div className="flex flex-col mr-6">
           <img
-            className="rounded w-[300px]"
+            className="rounded w-[250px]"
             alt="CulturaEduca"
             src={culturaeduca}
           />
@@ -27,11 +28,6 @@ function Projects() {
               Main technologies:
             </p>
             <div className="flex flex-wrap justify-end">
-              <img
-                className="p-1 m-1 h-20 bg-salmon rounded"
-                alt="python"
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
-              />
               <img
                 className="p-1 m-1 h-20 bg-salmon rounded"
                 alt="django"
@@ -57,10 +53,10 @@ function Projects() {
         </div>
       </div>
 
-      <div className="flex flex-wrap max-w-4xl my-12 p-3 bg-blue rounded">
+      <div className="flex flex-wrap max-w-4xl mb-16 p-3 bg-blue rounded">
         <div className="flex flex-col mr-6">
           <img
-            className="rounded w-[300px]"
+            className="rounded w-[250px]"
             alt="Robot of Love"
             src={robot_of_love}
           />
@@ -89,6 +85,44 @@ function Projects() {
                 className="p-1 m-1 h-20 bg-salmon rounded"
                 alt="selenium"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap max-w-4xl mb-8 p-3 bg-blue rounded">
+        <div className="flex flex-col mr-6">
+          <img className="rounded w-[250px]" alt="Portfolio" src={portfolio} />
+          <p className="text-center p-1 font-bold text-[30px]">My Portfolio</p>
+        </div>
+
+        <div className="flex flex-col flex-1 mt-2 justify-between">
+          <p className="text-[22px]">
+            This portfolio was developed using React, a Javascript library, in
+            addition to the Vite build tool. For styling, the option chosen was
+            Tailwind, a CSS framework.
+          </p>
+
+          <div className="flex flex-col mt-6">
+            <p className="font-bold text-[24px] text-right">
+              Main technologies:
+            </p>
+            <div className="flex flex-wrap justify-end">
+              <img
+                className="p-1 m-1 h-20 bg-salmon rounded"
+                alt="javascript"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              />
+              <img
+                className="p-1 m-1 h-20 bg-salmon rounded"
+                alt="react"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+              />
+              <img
+                className="p-1 m-1 h-20 bg-salmon rounded"
+                alt="tailwindcss"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
               />
             </div>
           </div>
