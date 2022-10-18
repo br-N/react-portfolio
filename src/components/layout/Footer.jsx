2ecc2562da64aa241462db7bb54df8832378098b
br-N <brenobl2000@gmail.com>
2022-10-18 14:01:23 -0300

    Ajustes no Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,26 +2,11 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center p-2">
-      <p>
+    <footer className="flex items-center justify-center mt-12 p-1">
+      <p className="text-[20px]">
         Developed by <span className="font-bold">Breno Lambertini</span> &copy;
         2022
       </p>
-      <ul className="flex mt-2">
-        <li className="mr-4">
-          <a
-            href="https://www.linkedin.com/in/brenolambertini/"
-            target="_blank"
-          >
-            <FaLinkedin size={20} />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/br-N/" target="_blank">
-            <FaGithub size={20} />
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 }
