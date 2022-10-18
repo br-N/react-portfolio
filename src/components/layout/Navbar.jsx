f7abd7f63b619f679b02cd38d035aa9870188070
br-N <brenobl2000@gmail.com>
2022-10-18 18:31:01 -0300

    Ajustes na estilização da Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,25 +2,53 @@ import { Link } from "react-router-dom";
 
 import Container from "./Container";
 
+import {
+  AiOutlineHome,
+  AiOutlineInfoCircle,
+  AiOutlineTool,
+  AiOutlineHourglass,
+} from "react-icons/ai";
+
 function Navbar() {
   return (
-    <nav className="p-2">
+    <nav className="mb-8 p-1 bg-blue">
       <Container>
-        <div className="font-cinzel font-extrabold text-xl">
-          <p>Breno Lambertini</p>
-        </div>
-        <ul className="flex">
-          <li className="mr-4">
-            <Link to="/">Home</Link>
+        <p className="font-cinzel font-extrabold text-[24px] ml-8">
+          <Link to="/">Breno Lambertini</Link>
+        </p>
+
+        <ul className="flex flex-wrap text-[18px]">
+          <li className="mr-8">
+            <Link to="/">
+              <div className="flex items-center">
+                <AiOutlineHome size={24} className="mr-1" />
+                Home
+              </div>
+            </Link>
           </li>
-          <li className="mr-4">
-            <Link to="/about">About</Link>
+          <li className="mr-8">
+            <Link to="/about">
+              <div className="flex items-center">
+                <AiOutlineInfoCircle size={24} className="mr-1" />
+                About
+              </div>
+            </Link>
           </li>
-          <li className="mr-4">
-            <Link to="/projects">Projects</Link>
+          <li className="mr-8">
+            <Link to="/projects">
+              <div className="flex items-center">
+                <AiOutlineTool size={24} className="mr-1" />
+                Projects
+              </div>
+            </Link>
           </li>
-          <li className="mr-4">
-            <Link to="/humanoid">Save The Humanoid</Link>
+          <li className="mr-8">
+            <Link to="/humanoid">
+              <div className="flex items-center">
+                <AiOutlineHourglass size={24} className="mr-1" />
+                Save The Humanoid
+              </div>
+            </Link>
           </li>
         </ul>
       </Container>
