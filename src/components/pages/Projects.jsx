dfa6cfe1e7a846d004d66ad62cfdb282ab13204f
br-N <brenobl2000@gmail.com>
2022-10-28 18:14:56 -0300

    Criação de arquivo de estilos para a Projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,3 +1,5 @@
+import styles from "./Projects.module.css";
+
 import culturaeduca from "../../img/culturaeduca.png";
 import robot_of_love from "../../img/robot-of-love.png";
 import portfolio from "../../img/portfolio.png";
@@ -5,37 +7,35 @@ import portfolio from "../../img/portfolio.png";
 function Projects() {
   return (
     <section className="flex flex-col items-center w-screen gap-16">
-      <div className="flex flex-col sm:flex-row max-w-4xl p-3 gap-6 bg-blue rounded">
-        <div className="flex flex-col justify-center items-center">
-          <img className="rounded w-60" alt="Portfolio" src={portfolio} />
+      <div className={styles.main_div}>
+        <div className={styles.left_div}>
+          <img className={styles.img} alt="Portfolio" src={portfolio} />
 
-          <h2 className="font-bold text-3xl">My Portfolio</h2>
+          <h2 className={styles.title}>My Portfolio</h2>
         </div>
 
-        <div className="flex flex-col flex-1 justify-between gap-6">
-          <p className="text-xl">
+        <div className={styles.right_div}>
+          <p className={styles.text}>
             This portfolio was developed using React, a Javascript library, in
             addition to the Vite build tool. For styling, the option chosen was
             Tailwind, a CSS framework.
           </p>
 
-          <div className="flex flex-col">
-            <p className="font-semibold text-2xl text-right">
-              Main technologies:
-            </p>
-            <div className="flex flex-wrap justify-end">
+          <div className={styles.tech_div}>
+            <p className={styles.tech_text}>Main technologies:</p>
+            <div className={styles.skills_div}>
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="javascript"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="react"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="tailwindcss"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
               />
@@ -44,15 +44,15 @@ function Projects() {
         </div>
       </div>
 
-      <div className="flex flex-col sm:flex-row max-w-4xl p-3 gap-6 bg-blue rounded">
-        <div className="flex flex-col justify-center items-center">
-          <img className="rounded w-60" alt="CulturaEduca" src={culturaeduca} />
+      <div className={styles.main_div}>
+        <div className={styles.left_div}>
+          <img className={styles.img} alt="CulturaEduca" src={culturaeduca} />
 
-          <h2 className="font-bold text-3xl">CulturaEduca</h2>
+          <h2 className={styles.title}>CulturaEduca</h2>
         </div>
 
-        <div className="flex flex-col flex-1 justify-between gap-6">
-          <p className="text-xl">
+        <div className={styles.right_div}>
+          <p className={styles.text}>
             Software development at CulturaEduca, a platform maintained by
             InterSCity (USP) built especially with Django Framework, which
             focuses mainly on geolocation and information sharing in the context
@@ -66,28 +66,26 @@ function Projects() {
             </a>
           </p>
 
-          <div className="flex flex-col">
-            <p className="font-semibold text-2xl text-right">
-              Main technologies:
-            </p>
-            <div className="flex flex-wrap justify-end">
+          <div className={styles.tech_div}>
+            <p className={styles.tech_text}>Main technologies:</p>
+            <div className={styles.skills_div}>
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="django"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="postgresql"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="javascript"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="foundation"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/foundation/foundation-original-wordmark.svg"
               />
@@ -96,19 +94,15 @@ function Projects() {
         </div>
       </div>
 
-      <div className="flex flex-col sm:flex-row max-w-4xl p-3 gap-6 bg-blue rounded">
-        <div className="flex flex-col justify-center items-center">
-          <img
-            className="rounded w-60"
-            alt="Robot of Love"
-            src={robot_of_love}
-          />
+      <div className={styles.main_div}>
+        <div className={styles.left_div}>
+          <img className={styles.img} alt="Robot of Love" src={robot_of_love} />
 
-          <h2 className="font-bold text-3xl">Robot of Love</h2>
+          <h2 className={styles.title}>Robot of Love</h2>
         </div>
 
-        <div className="flex flex-col flex-1 justify-between gap-6">
-          <p className="text-xl">
+        <div className={styles.right_div}>
+          <p className={styles.text}>
             Robot of Love is a bot built with Python and Selenium that likes
             every post and follows every post owner of a specific URL, which can
             be either a tag or a person. And user can choose if he want only to
@@ -122,18 +116,16 @@ function Projects() {
             </a>
           </p>
 
-          <div className="flex flex-col">
-            <p className="font-semibold text-2xl text-right">
-              Main technologies:
-            </p>
-            <div className="flex flex-wrap justify-end">
+          <div className={styles.tech_div}>
+            <p className={styles.tech_text}>Main technologies:</p>
+            <div className={styles.skills_div}>
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="python"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
               />
               <img
-                className="p-1 m-1 h-20 bg-salmon rounded transition duration-300 ease-out hover:scale-110"
+                className={styles.skills_icon}
                 alt="selenium"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
               />
