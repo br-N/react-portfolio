cfa99df50baa90e4b0200ca633ea0abf16e7a997
br-N <brenobl2000@gmail.com>
2023-01-27 23:27:10 -0300

    Adição de novo projeto e Ajustes

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,8 +2,9 @@ import styles from "./css/Projects.module.css";
 
 import portfolio from "../../img/portfolio.png";
 import culturaeduca from "../../img/culturaeduca.png";
-import robot_of_love from "../../img/robot-of-love.png";
+import angular_project from "../../img/angular-project.jpg";
 import react_project from "../../img/react-project.png";
+import robot_of_love from "../../img/robot-of-love.png";
 
 function Projects() {
   return (
@@ -17,9 +18,9 @@ function Projects() {
 
         <div className={styles.right_div}>
           <p className={styles.text}>
-            This portfolio was developed from scratch using React, a Javascript
-            library, in addition to the Vite build tool. For styling, the option
-            chosen was Tailwind, a CSS framework.{" "}
+            &emsp;This portfolio was designed and developed from scratch using
+            React, a Javascript library, in addition to the Vite build tool. For
+            styling, the option chosen was Tailwind, a CSS framework.{" "}
             <a
               className="underline"
               href="https://github.com/br-N/react-portfolio"
@@ -56,15 +57,20 @@ function Projects() {
         <div className={styles.left_div}>
           <img className={styles.img} alt="CulturaEduca" src={culturaeduca} />
 
-          <h2 className={styles.title}>CulturaEduca</h2>
+          <h2 className={styles.title}>Cultura Educa</h2>
         </div>
 
         <div className={styles.right_div}>
           <p className={styles.text}>
-            Software development at CulturaEduca, a platform maintained by
-            InterSCity (USP) built especially with Django Framework, which
-            focuses mainly on geolocation and information sharing in the context
-            of culture and education in cities.{" "}
+            &emsp;Totally open-source platform maintained by InterSCity (USP),
+            which focuses on geolocation and information sharing in the context
+            of culture and education in cities. Main contributions: 1) Front-end
+            and back-end development of Textual Search (with queries in more
+            than 20 different databases, containing tens of thousands of data,
+            in addition to the use of pagination, triggers, materialized views,
+            among others. 2) Refactoring of the geolocation parts, which uses
+            OpenStreetMap map, JavaScript library OpenLayers, in addition to the
+            Correios API and Nominatim API.{" "}
             <a
               className="underline"
               href="https://culturaeduca.cc/"
@@ -79,16 +85,6 @@ function Projects() {
             <div className={styles.skills_div}>
               <img
                 className={styles.skills_icon}
-                alt="django"
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"
-              />
-              <img
-                className={styles.skills_icon}
-                alt="postgresql"
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
-              />
-              <img
-                className={styles.skills_icon}
                 alt="javascript"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
               />
@@ -97,6 +93,16 @@ function Projects() {
                 alt="foundation"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/foundation/foundation-original-wordmark.svg"
               />
+              <img
+                className={styles.skills_icon}
+                alt="django"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"
+              />
+              <img
+                className={styles.skills_icon}
+                alt="postgresql"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
+              />
             </div>
           </div>
         </div>
@@ -104,20 +110,37 @@ function Projects() {
 
       <div className={styles.main_div}>
         <div className={styles.left_div}>
-          <img className={styles.img} alt="Robot of Love" src={robot_of_love} />
+          <img
+            className={styles.img}
+            alt="Angular Project"
+            src={angular_project}
+          />
 
-          <h2 className={styles.title}>Robot of Love</h2>
+          <h2 className={styles.title}>Angular Project</h2>
         </div>
 
         <div className={styles.right_div}>
           <p className={styles.text}>
-            Robot of Love is a bot built with Python and Selenium that likes
-            every post and follows every post owner of a specific URL, which can
-            be either a tag or a person. And user can choose if he want only to
-            like, only to follow or both.{" "}
+            &emsp;Full stack project developed in Angular that consists of a
+            website similar to Instagram, in which it is possible to share
+            moments having the name of the moment, a description and an image.
+            Moments are stored in a relational database (SQLite), using an
+            RESTful API built in AdonisJS{" "}
             <a
               className="underline"
-              href="https://github.com/br-N/robot-of-love"
+              href="https://github.com/br-N/api-rest-adonisjs"
+              target="_blank"
+            >
+              (Check here!)
+            </a>
+            . Furthermore, it is allowed to edit the data of each moment or
+            delete it. The moments are shown on the Home page, and it is also
+            possible to add comments to the moments, with the user name and the
+            comment itself as fields, and they are added to the database as
+            well.{" "}
+            <a
+              className="underline"
+              href="https://github.com/br-N/curso-angular-projeto"
               target="_blank"
             >
               Check here!
@@ -129,13 +152,23 @@ function Projects() {
             <div className={styles.skills_div}>
               <img
                 className={styles.skills_icon}
-                alt="python"
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+                alt="typescript"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
               />
               <img
                 className={styles.skills_icon}
-                alt="selenium"
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
+                alt="angular"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original-wordmark.svg"
+              />
+              <img
+                className={styles.skills_icon}
+                alt="adonisjs"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/adonisjs/adonisjs-original-wordmark.svg"
+              />
+              <img
+                className={styles.skills_icon}
+                alt="sqlite"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg"
               />
             </div>
           </div>
@@ -144,17 +177,20 @@ function Projects() {
 
       <div className={styles.main_div}>
         <div className={styles.left_div}>
-          <img className={styles.img} alt="Portfolio" src={react_project} />
+          <img className={styles.img} alt="React Project" src={react_project} />
 
           <h2 className={styles.title}>React Project</h2>
         </div>
 
         <div className={styles.right_div}>
           <p className={styles.text}>
-            Project developed in React whose objective is to manage projects of
-            different budgets and categories, being possible to create services
-            for each project, automatically updating the budget. It uses all
-            CRUD concepts: Create, Read, Update and Delete.{" "}
+            &emsp;Full stack project developed in React whose objective is to
+            manage projects in general, having as fields a name, budget and
+            category. Projects are stored in a non-relational database
+            (MongoDB), using the JSON Server module that simulates a REST API.
+            Furthermore, it is possible to edit the project, being able to add
+            services with a pre-established cost, automatically updating the
+            total value used of the project budget.{" "}
             <a
               className="underline"
               href="https://github.com/br-N/curso-react-projeto"
@@ -176,6 +212,51 @@ function Projects() {
                 className={styles.skills_icon}
                 alt="react"
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+              />
+              <img
+                className={styles.skills_icon}
+                alt="mongodb"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.main_div}>
+        <div className={styles.left_div}>
+          <img className={styles.img} alt="Robot of Love" src={robot_of_love} />
+
+          <h2 className={styles.title}>Robot of Love</h2>
+        </div>
+
+        <div className={styles.right_div}>
+          <p className={styles.text}>
+            &emsp;Automation for Instagram developed in Python and Selenium that
+            likes every post and follows every post owner of a specific URL,
+            which can be either a tag or a person. And the user can choose if he
+            want only to like, only to follow or both.{" "}
+            <a
+              className="underline"
+              href="https://github.com/br-N/robot-of-love"
+              target="_blank"
+            >
+              Check here!
+            </a>
+          </p>
+
+          <div className={styles.tech_div}>
+            <p className={styles.tech_text}>Main technologies:</p>
+            <div className={styles.skills_div}>
+              <img
+                className={styles.skills_icon}
+                alt="python"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+              />
+              <img
+                className={styles.skills_icon}
+                alt="selenium"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
               />
             </div>
           </div>
