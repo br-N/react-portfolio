90ec3628c729f28d1483c24dac884bbdc29c767c
Breno Lambertini <brenobl2000@gmail.com>
2023-08-12 17:36:55 -0300

    Alteração ano Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="flex items-center justify-center mt-12 p-2 text-base">
       <p>
         Designed and developed by{" "}
-        <span className="font-semibold">Breno Lambertini</span> &copy; 2022
+        <span className="font-semibold">Breno Lambertini</span> &copy; 2023
       </p>
     </footer>
   );
